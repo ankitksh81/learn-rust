b353e34c58e99c751af7e6a98d9ae46f30670de5
ankitksh81 <ankitksh81@gmail.com>
2021-02-19 19:08:38 +0530

    operators

---
+++ types-var/operators.rs
@@ -0,0 +1,20 @@
+use std::mem; // for inbuilt functions.
+
+fn main() {
+  
+  // arithmetic operators + - * / %.
+
+  let mut a = 2 + 3 * 4; // + - *
+  println!("{}", a);
+
+  // increment & decrement opertors are not supported.
+
+  a = a + 1; // a++ or a-- not supported.
+
+  // mathematical functions
+
+  let a_cubed = i32::pow(a, 3); // pow function used to find power of a number.
+  // var = type::fun(param1, param2);
+
+
+}
